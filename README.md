0e9360d1ae3d3d0ce92fda385837157a68a4f069
rxfe.a <104333985+rxfe-a@users.noreply.github.com>
2023-12-09 10:36:29 -0800

    Bare

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # BareDeployable
 A deployable version of the bare server node
+
+Deploy it by using your brain (npm install, npm start)
---
+++ index.js
@@ -0,0 +1,29 @@
+import { createBareServer } from "@tomphttp/bare-server-node";
+import http from 'node:http';
+const PORT = process.env.PORT || 8080;
+
+////////////////////////////////////////
+const bareServer = createBareServer('/')
+const server = http.createServer();
+server.on('request', (req, res) => {
+    if (bare.shouldRoute(req)) {
+		bare.routeRequest(req, res);
+	} else {
+        res.writeHead(400);
+		res.end('400 Bad Request');
+	}
+});
+
+server.on('upgrade', (req, socket, head) => {
+	if (bare.shouldRoute(req, socket, head)) {
+		bare.routeUpgrade(req, socket, head);
+	} else {
+		socket.end();
+	}
+});
+
+server.on('listening', () => {
+	console.log(`Bare server started on port ${PORT}`)
+});
+
+server.listen({ port: PORT });
